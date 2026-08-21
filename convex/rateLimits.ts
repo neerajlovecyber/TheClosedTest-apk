3223b7db9067a501a2da466398aff5cb504db03e
neerajlovecyber <neerajlovecyber@gmail.com>
2026-08-21 10:32:44 +0530

    refactor: remove convex and related aws-sdk dependencies from the project

--- convex/rateLimits.ts
+++
@@ -1,7 +0,0 @@
-import { RateLimiter, HOUR } from "@convex-dev/rate-limiter";
-import { components } from "./_generated/api";
-
-export const rateLimiter = new RateLimiter(components.rateLimiter, {
-    checkIn: { kind: "fixed window", rate: 1, period: 2 * HOUR },
-    onePerMin: { kind: "fixed window", rate: 3, period: 60 * 1000 },
-});
